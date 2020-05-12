cf7451e15bc3e7e7a2396a96963c6bb638937944
Joppe <joppe.klaver@gmail.com>
2020-05-12 10:28:23 +0200

    Bootstrap toegevoegd

--- Front-End/loginpage/src/app/app-routing.module.ts
+++ Front-End/loginpage/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login-page', pathMatch: 'full' },
   { path: 'login-page', component: LoginPageComponent },
-  { path: 'register-page', component: RegisterPageComponent}
+  { path: 'register-page', component: RegisterPageComponent},
+  { path: 'navigation-bar', component: NavigationBarComponent}
 ];
 
 @NgModule({
--- Front-End/loginpage/src/app/app.module.ts
+++ Front-End/loginpage/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
-    RegisterPageComponent
+    RegisterPageComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
