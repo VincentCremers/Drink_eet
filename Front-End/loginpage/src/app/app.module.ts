c54e9aebd3d97b0e039b3aeeb5c05defb323dad0
Joppe <joppe.klaver@gmail.com>
2020-05-16 09:52:23 +0200

    Feedback, wachtwoord vergeten en header toegevoegd

--- Front-End/loginpage/src/app/app.module.ts
+++ Front-End/loginpage/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { RegisterPageComponent } from './register-page/register-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ForgotPassComponent } from './forgot-pass/forgot-pass.component';
+import { FeedbackPageComponent } from './feedback-page/feedback-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
     RegisterPageComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    ForgotPassComponent,
+    FeedbackPageComponent
   ],
   imports: [
     BrowserModule,
