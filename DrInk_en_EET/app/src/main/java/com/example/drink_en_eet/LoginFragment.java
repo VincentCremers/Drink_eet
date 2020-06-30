e78db553a19a862a8f17819adb692c0420631ec1
Joppert123 <44192052+Joppert123@users.noreply.github.com>
2020-06-30 10:30:04 +0200

    Push voor merge

--- DrInk_en_EET/app/src/main/java/com/example/drink_en_eet/LoginFragment.java
+++ DrInk_en_EET/app/src/main/java/com/example/drink_en_eet/LoginFragment.java
@@ -1,12 +1,10 @@
 package com.example.drink_en_eet;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -19,8 +17,11 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
 
         View view = inflater.inflate(R.layout.activity_login, container, false);
 
-        Button button = (Button) view.findViewById(R.id.button4);
-        button.setOnClickListener(this);
+        Button registreer_button = (Button) view.findViewById(R.id.registreer);
+        Button wwvergeten_button = (Button) view.findViewById(R.id.wwvergeten);
+
+        registreer_button.setOnClickListener(this);
+        wwvergeten_button.setOnClickListener(this);
 
 
         return view;
@@ -29,10 +30,14 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.button4:
+            case R.id.registreer:
                 getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                         new RegisterFragment()).addToBackStack("tag").commit();
                 break;
+
+            case R.id.wwvergeten:
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ForgetFragment()).addToBackStack("tag").commit();
         }
     }
 }
